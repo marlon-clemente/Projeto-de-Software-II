b90b9babdb7d42e82d63b276703b95f6002f2727
Marlon Clemente <marlon.klemente@gmail.com>
2020-11-14 00:23:34 -0300

    Map view Adjusted

--- src/pages/Schools.tsx
+++ src/pages/Schools.tsx
@@ -47,12 +47,7 @@ const Schools: React.FC = () => {
       <Map
         center={[-29.6899828,-53.8080099]}
         zoom={15}
-        style={{ width: '100%', height:280 }}
-        dragging = {false}
-        touchZoom={false}
-        zoomControl={false}
-        scrollWheelZoom={false}
-        doubleClickZoom={false}
+        style={{ width: '100%', height:'100%' }}
       >
         <TileLayer url={
           `https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`
